51e03a119b13a407621abc3f158c8d82d0305b5d
akatzfey <katzfeyalex@gmail.com>
2022-03-01 17:27:28 -0800

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Alexander Katzfey on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
